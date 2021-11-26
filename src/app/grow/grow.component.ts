977c223a8b214e45f795e1a94f81d9753dd730cf
lama17 <lama17@uci.edu>
2021-11-26 01:24:01 -0800

    potions

--- src/app/grow/grow.component.ts
+++ src/app/grow/grow.component.ts
@@ -17,16 +17,18 @@ export class GrowComponent implements OnInit {
   n1:Array<string>;
   n2:Array<string>;
   n3:Array<string>;
+  n4:Array<string>;
   subscription:Subscription;
   
   constructor(private data: MenuDataService){}
 
 
   ngOnInit(): void {
-    this.data.changeName(["Surviving2020", "#F0B8BF", "surviving2020"], ["Project Cupid", "#CFB5D0", "project-cupid"], ["Stardew Journal", "#F5B971", "stardew-journal"])
+    this.data.changeName(["Project Cupid", "#CFB5D0", "project-cupid"], ["Puzzling Potions","#F5B971", "puzzling-potions"], ["Stardew Journal", "#CFB5D0", "stardew-journal"], ["Surviving2020", "#F0B8BF", "surviving2020"])
     this.subscription = this.data.currentN1.subscribe(n1 => this.n1 = n1)
     this.subscription = this.data.currentN2.subscribe(n2 => this.n2 = n2)
     this.subscription = this.data.currentN3.subscribe(n3 => this.n3 = n3)
+    this.subscription = this.data.currentN4.subscribe(n4 => this.n4 = n4)
   }
 
 
--- src/app/puzzlingpotions/puzzlingpotions.component.ts
+++ src/app/puzzlingpotions/puzzlingpotions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MenuDataService } from '../menu-data.service';
 
 @Component({
   selector: 'app-puzzlingpotions',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuzzlingpotionsComponent implements OnInit {
 
-  constructor() { }
+  n1:Array<string>;
+  n2:Array<string>;
+  n3:Array<string>;
+  n4:Array<string>;
+  subscription:Subscription;
+
+  constructor(private data: MenuDataService) { }
 
   ngOnInit(): void {
+    this.data.changeName(["Stardew Journal", "#CFB5D0", "stardew-journal"], ["Surviving2020", "#F0B8BF", "surviving2020"], ["grow", "#A3DDCB", "grow"], ["Project Cupid", "#CFB5D0", "project-cupid"])
+    this.subscription = this.data.currentN1.subscribe(n1 => this.n1 = n1)
+    this.subscription = this.data.currentN2.subscribe(n2 => this.n2 = n2)
+    this.subscription = this.data.currentN3.subscribe(n3 => this.n3 = n3)
+    this.subscription = this.data.currentN4.subscribe(n4 => this.n4 = n4)
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
+
